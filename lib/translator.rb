2ac5f2b29b30bb6744eb99cf6ca20d9c38553a81
Jamal Shareef <theepie@hotmail.com>
2019-04-12 00:45:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,22 +2,33 @@
 require 'pry'
 require 'yaml'
 
-emoticons = YAML.load_file('lib/emoticons.yml')
-puts emoticons
-
 def load_library
   # code goes here
   library = YAML.load_file('emoticons.yml')
   library
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(emoticon)
   # code goes here
   library = load_library
-  binding.pry
-  library
+  library.each do |name, array|
+    if array[0] == emoticon
+      return array[1]
+    else
+      nil
+    end
+  end
 end
 
 def get_english_meaning
   # code goes here
+  
+  library = load_library
+  library.each do |name, array|
+    if array[1] == emoticon
+      return name
+    else
+      nil
+    end
+  end
 end
